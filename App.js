73c56d7bb75d2342a44fb03ec05fb69866dffae5
adibest <ahmadjayadibya@gmail.com>
2019-07-11 14:32:58 +0700

    like bisa

--- App.js
+++ App.js
@@ -15,23 +15,24 @@ export default class App extends Component {
           <View style={styles.content}>
           <ScrollView>
             <Article 
-              title="judul" 
-              sentences="elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus" 
+              title="@el_adib" 
+              sentences="elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus"
+              image="uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'" 
             />
             <Article 
-              title="judul" 
+              title="@fiassan" 
               sentences="vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet" 
             />
             <Article 
-              title="judul" 
+              title="@kap" 
               sentences="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae sapien pellentesque habitant morbi tristique senectus et netus et. Amet justo donec enim diam vulputate ut pharetra sit. Turpis in eu mi bibendum neque egestas congue quisque. Suspendisse ultrices gravida dictum fusce ut. Eget gravida cum sociis natoque. Feugiat nisl pretium fusce id. Malesuada pellentesque elit eget gravida cum sociis natoque. Aliquet enim tortor at auctor urna nunc id cursus. Eget arcu dictum varius duis at consectetur lorem donec. Eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Dolor morbi non arcu risus quis varius quam quisque id." 
             />
             <Article 
-              title="judul" 
+              title="@horotoyo" 
               sentences="elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet" 
             />
             <Article 
-              title="judul" 
+              title="@addthoriq" 
               sentences="elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus pretium lectus quam id leo in vitae turpis massa sed elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis urna id volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim convallis aenean et tortor at risus viverra adipiscing at in tellus integer feugiat scelerisque varius morbi enim" 
             />
           </ScrollView>
--- Article.js
+++ Article.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image, TouchableWithoutFeedback} from 'react-native';
 
 export default class Article extends Component {
+
+	state = {
+		like: false,
+	};
+
+	likeHandle() {
+		this.setState({
+			like: !this.state.like
+		});
+	}
 
 	render() {
 
 		return(
 				<View style={styles.wrapper}>
-					<Text style={styles.title}>
-						{ this.props.title }
-					</Text>
+					<Image
+						style={{width: 210, height: 210}}
+						source={ this.props.image }
+					/>
+					<View style={{flexDirection: 'row'}}>
+						<TouchableWithoutFeedback
+							onPress={() => this.likeHandle()}
+						>
+							<Image
+								style={{width: 20, height: 20}}
+								source={{uri: this.state.like ? 'https://cdn2.iconfinder.com/data/icons/pittogrammi/142/80-512.png' : 'http://cdn.onlinewebfonts.com/svg/img_202778.png'}}
+							/>
+						</TouchableWithoutFeedback>
+						<Text style={styles.title}>
+							{ this.props.title }
+						</Text>
+					</View>
 					<Text style={styles.sentences}>
 						{ this.props.sentences }
 					</Text>
@@ -28,7 +52,7 @@ const styles = StyleSheet.create({
 		borderBottomColor: '#aaaeee',
 	},
 	title: {
-		fontSize: 24,
+		fontSize: 18,
 	},
 	sentences: {
 		fontSize: 12,
